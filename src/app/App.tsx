6665b70126155c6e25d762efc9f461e00ec5c430
Alexandrov Georgy <g.aleksandrov116@yandex.ru>
2026-07-28 12:40:03 +0300

    feat: - настройка темы - переключатель - store theme.ts

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,14 +2,19 @@ import { Router } from './routing';
 import { GlobalStyles } from './styles';
 import { ThemeProvider } from 'styled-components';
 import { theme } from '@/shared/styles';
+import { useThemeStore } from '@/app/store';
+import ThemeToggle from '@/features/toogle-theme';
 
-const App = () => (
-  <>
-    <ThemeProvider theme={theme.light}>
+const App = () => {
+  const { isDark } = useThemeStore();
+
+  return (
+    <ThemeProvider theme={isDark ? theme.dark : theme.light}>
       <GlobalStyles />
+      <ThemeToggle />
       <Router />
     </ThemeProvider>
-  </>
-);
+  );
+};
 
 export default App;
---
+++ src/shared/assets/index.ts
@@ -0,0 +1,4 @@
+import SunSvg from './icons/sun.svg?react';
+import MoonSvg from './icons/moon.svg?react';
+
+export { SunSvg, MoonSvg };
